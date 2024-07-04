99d67cb29c7f434f0f30613e7cafe9c9d3f11931
mohitgautam15 <98653083+mohitgautam15@users.noreply.github.com>
2024-07-04 10:14:30 +0530

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,28 @@
+package com.example.healthcare.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String speciality;
+}
